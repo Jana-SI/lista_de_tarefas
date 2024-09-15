281bfdebe89827c5c401903f4a806b54db79759e
Jana-SI <janaina.adolfo.ds@gmail.com>
2024-09-15 17:49:17 -0300

    criação da função editar tarefa

--- src/App.js
+++ src/App.js
@@ -1,45 +1,89 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [tarefa, setTarefa] = useState("");
+  const [listaDeTarefas, setListaDeTarefas] = useState([]);
+  const [editarIndex, setEditarIndex] = useState(null);
+  const [novaTarefa, setNovaTarefa] = useState("");
 
-    const [tarefa, setTarefa] = useState();
-    const [listaDeTarefas, setListaDeTarefas] = useState([]);
+  const atualizaTarefa = (e) => {
+    setTarefa(e.target.value);
+  };
 
-    const atualizaTarefa = (e) => {
-     setTarefa(e.target.value);
-    };
+  const adicionarTarefa = () => {
+    if (tarefa.trim() !== "") {
+      setListaDeTarefas([tarefa, ...listaDeTarefas]);
+      setTarefa("");
+    }
+  };
 
-    const adicionarTarefa = () => {
-      if(tarefa.trim() !== ''){
-        setListaDeTarefas([tarefa, ...listaDeTarefas]);
-        setTarefa('');
-      }
-    };
+  const removerTarefa = (index) => {
+    const novaLista = listaDeTarefas.filter((_, i) => i !== index);
+    setListaDeTarefas(novaLista);
+  };
 
-    const removerTarefa = (index) => {
-      const novaLista = listaDeTarefas.filter((_, i) => i !== index);
+  const enterPressionado = (e) => {
+    if (e.key === "Enter") {
+      adicionarTarefa();
+      salvarEdicao();
+    }
+  };
+
+  const iniciarEdicao = (index) => {
+    setEditarIndex(index);
+    setNovaTarefa(listaDeTarefas[index]);
+  };
+
+  const salvarEdicao = () => {
+    if (novaTarefa.trim() !== "") {
+      const novaLista = listaDeTarefas.map((tarefa, i) =>
+        i === editarIndex ? novaTarefa : tarefa
+      );
+
       setListaDeTarefas(novaLista);
+      setEditarIndex(null);
+      setNovaTarefa("");
     }
-
-    const enterPressionado = (e) => {
-      if(e.key === "Enter"){
-        adicionarTarefa();
-      }
-    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Lista de Tarefas</h1>
 
-        <input type='text' value={tarefa} onChange={atualizaTarefa} onKeyDown={enterPressionado} placeholder='Digite sua tarefa aqui'/>
+        <input
+          type="text"
+          value={tarefa}
+          onChange={atualizaTarefa}
+          onKeyDown={enterPressionado}
+          placeholder="Digite sua tarefa aqui"
+        />
 
         <button onClick={adicionarTarefa}>Adicionar</button>
 
         <ul>
           {listaDeTarefas.map((tarefa, index) => (
-            <li key={index}>{tarefa} <button onClick={()=> removerTarefa(index)}>X</button></li>
+            <li key={index}>
+              {editarIndex === index ? (
+                <>
+                  <input
+                    type="text"
+                    value={novaTarefa}
+                    onKeyDown={enterPressionado}
+                    onChange={(e) => setNovaTarefa(e.target.value)}
+                  />
+
+                  <button onClick={salvarEdicao}>Salvar</button>
+                </>
+              ) : (
+                <>
+                  {tarefa}
+                  <button onClick={() => iniciarEdicao(index)}>Editar</button>
+                  <button onClick={() => removerTarefa(index)}>X</button>
+                </>
+              )}
+            </li>
           ))}
         </ul>
       </header>
